633a3c0ac02fe87e86dca00ed68f8d547e6f1ce1
Shivam Yadav <115492003+Shivamyadav4344@users.noreply.github.com>
2022-11-17 11:47:57 +0530

    Update itertools2.py

--- itertools2.py
+++ itertools2.py
@@ -1,17 +1,17 @@
-from itertools import count
+from itertools2 import count
 for i in count(10):
     print(i)
     if i == 15:
         break
     
-from itertools import cycle
+from itertools2 import cycle
 a = [1,2,3,4]
 for i in cycle(a):
     print(i)
     if i == 3:
         break
     
-from itertools import repeat
+from itertools2 import repeat
 a = [1,2,3,4]
 for i in repeat(3):
     print(i)
